226911d8117cee4866cc11f976274a8b9505c128
lebnic <nleblanc@fibrenoire.ca>
2017-08-05 14:20:04 -0400

    4: added current location to map

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -21,8 +21,31 @@ export class MapComponent implements OnInit {
 
 
     this.mymap.on('click', (ev) => {
-      L.marker([ev.latlng.lat, ev.latlng.lng]).addTo(this.mymap)
+      L.marker(ev.latlng).addTo(this.mymap)
         .bindPopup('<img src="/assets/spot.jpg" style="width:300px;display:inline-block;margin:5px 0 2px 5px"/>');
+    });
+
+    this.mymap.locate();
+
+    this.mymap.on('locationfound', (ev) => {
+      this.mymap.setView(ev.latlng, 17);
+      let radius = ev.accuracy / 2;
+
+      L.marker(
+        ev.latlng,
+        {
+          icon: L.icon({
+            iconUrl: '/assets/spotty.svg',
+            iconSize: [25, 25]
+          })
+        }
+      ).addTo(this.mymap);
+
+      L.circle(ev.latlng, radius).addTo(this.mymap);
+    });
+
+    this.mymap.on('locationerror', (ev) => {
+      alert(ev.message);
     });
   }
 }
