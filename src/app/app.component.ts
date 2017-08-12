7254ed8c0a5800d2cf55cb14791a82460914b2bd
lebnic <nleblanc@fibrenoire.ca>
2017-08-12 12:50:08 -0400

    database values can only be read if loged in with google

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
+import { AuthService } from './providers/auth.service';
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 import { Component } from '@angular/core';
 import { Angulartics2, Angulartics2GoogleAnalytics } from 'angulartics2';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,11 +10,22 @@ import { Angulartics2, Angulartics2GoogleAnalytics } from 'angulartics2';
 export class AppComponent {
   spots: FirebaseListObservable<any[]>;
 
-  constructor(
+  constructor(public authService: AuthService,
     db: AngularFireDatabase,
     angulartics2: Angulartics2,
     angulartics2GoogleAnalytics: Angulartics2GoogleAnalytics
   ) {
     this.spots = db.list('/spots');
+  }
+
+  login() {
+    this.authService.loginWithGoogle();
+    // https://medium.com/codingthesmartway-com-blog/angular-2-firebase-authentication-7593c565fa53
+    // https://youtu.be/tNCA1RdHrsM?t=32m40s
+    // https://github.com/angular/angularfire2/blob/master/docs/version-4-upgrade.md#removing-angularfire-for-modularity
+  }
+
+  logout() {
+    this.authService.logout();
   }
 }
